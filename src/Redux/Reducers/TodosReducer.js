15f1a8f4687b72f7367fa78fd855e3bd76c235e9
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-10-24 13:53:29 +0700

    Create todos reducers: addtodo, removetodo, completeTodo

---
+++ src/Redux/Reducers/TodosReducer.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [],
+};
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push(action.payload);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    completeTodo: (state, action) => {
+      state.todos = state.todos.map((todo) => {
+        if (todo.id === action.payload) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+    },
+  },
+});
+
+export const { addTodo, removeTodo, completeTodo } = todosSlice.actions;
+export default todosSlice.reducer;
