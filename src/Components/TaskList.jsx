44c0fa16ff95f89cfedb71c3cb3b9be485d2d2b1
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-10-24 20:02:08 +0700

    Access setFilter action to filter list notes when click filter, send props to component filter list, and show notes based on filter

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -1,23 +1,37 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import CardItem from "./CardItem";
 import FilterList from "./FilterList";
+import { setFilter } from "../Redux/Reducers/TodosReducer";
 
 export default function TaskList() {
-    const tasks = useSelector(state => state.todos.todos)
-    console.log(tasks)
+  const dispatch = useDispatch();
+  const tasks = useSelector((state) => state.todos.todos);
+  const filter = useSelector((state) => state.todos.filter);
+  console.log(tasks);
+  console.log(filter);
 
-    return (
-        <>
-            <FilterList />
-            <div className="row row-cols-1 row-cols-md-3 g-4 py-3">
-                {
-                    tasks.map((task) => {
-                        return (
-                            <CardItem key={task.id} task={task} />
-                        )
-                    })
-                }
-            </div>
-        </>
-    )
+  const handleFilter = (newFilter) => {
+    dispatch(setFilter(newFilter))
+  };
+
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === "Active") {
+      return !task.completed;
+    } else if (filter === "Completed") {
+      return task.completed;
+    } else {
+      return true;
+    }
+  });
+
+  return (
+    <>
+      <FilterList filter={filter} handleFilter={handleFilter}/>
+      <div className="row row-cols-1 row-cols-md-3 g-4 py-3">
+        {filteredTasks.map((task) => {
+          return <CardItem key={task.id} task={task} />;
+        })}
+      </div>
+    </>
+  );
 }
