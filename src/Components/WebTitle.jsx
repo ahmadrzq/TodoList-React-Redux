771ec4a83d4893f4af12abfbd31595e5e14643d0
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-10-24 13:56:03 +0700

    Create component for title website

---
+++ src/Components/WebTitle.jsx
@@ -0,0 +1,9 @@
+export default function WebTitle() {
+    return (
+        <div className="row">
+            <div className="col-12">
+                <div className='fs-1 text-center py-5'>Notefy V2</div>
+            </div>
+        </div>
+    )
+}
