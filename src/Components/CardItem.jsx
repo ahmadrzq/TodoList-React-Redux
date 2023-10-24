e4e7a8deb9c4febe3d1c1e532599eb0860f4949f
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-10-24 22:14:06 +0700

    Create handler to dispatch toggleEdit and UpdateTodo add handle button edit note

--- src/Components/CardItem.jsx
+++ src/Components/CardItem.jsx
@@ -1,7 +1,13 @@
 import { useDispatch } from "react-redux";
-import { completeTodo, removeTodo } from "../Redux/Reducers/TodosReducer";
+import {
+  completeTodo,
+  removeTodo,
+  toggleEdit,
+  updateTodo,
+} from "../Redux/Reducers/TodosReducer";
+import { useState } from "react";
 
-export default function CardItem({ task }) {
+export default function CardItem({ task, editingNoteId }) {
   const dispatch = useDispatch();
   // handle remove
   const handleRemove = (noteId) => {
@@ -11,30 +17,78 @@ export default function CardItem({ task }) {
   const handleComplete = (noteId) => {
     dispatch(completeTodo(noteId));
   };
+  //   handle edit
+  const handleEdit = (noteId) => {
+    dispatch(toggleEdit(noteId));
+  };
+  //   Update note
+  const [editedTitleNote, setEditedTitleNote] = useState(task.title);
+  const [editedNote, setEditedNote] = useState(task.note);
+  const handleUpdateNote = (noteId) => {
+    dispatch(
+      updateTodo({ id: noteId, title: editedTitleNote, note: editedNote })
+    );
+    handleEdit(null);
+  };
+
   return (
     <div className="col">
       <div className="card">
         <div className="card-body">
-          <h5
-            className={
-              task.completed
-                ? `card-text fs-3 fw-bolder text-decoration-line-through`
-                : `card-text fs-3 fw-bolder`
-            }
-          >
-            {task.title}
-          </h5>
-          <p
-            className={
-              task.completed
-                ? `card-text text-decoration-line-through`
-                : `card-text`
-            }
-          >
-            {task.note}
-          </p>
+          {task.id === editingNoteId ? (
+            <>
+              <input
+                type="text"
+                className="form-control mb-1"
+                value={editedTitleNote}
+                onChange={(e) => setEditedTitleNote(e.target.value)}
+                required
+              />
+              <textarea
+                rows={3}
+                type="text"
+                className="form-control mb-1"
+                value={editedNote}
+                onChange={(e) => setEditedNote(e.target.value)}
+                required
+              />
+            </>
+          ) : (
+            <>
+              <h5
+                className={
+                  task.completed
+                    ? `card-text fs-3 fw-bolder text-decoration-line-through text-danger`
+                    : `card-text fs-3 fw-bolder`
+                }
+              >
+                {task.title}
+              </h5>
+              <p
+                className={
+                  task.completed
+                    ? `card-text text-decoration-line-through text-danger`
+                    : `card-text`
+                }
+              >
+                {task.note}
+              </p>
+            </>
+          )}
           <div className="d-flex column-gap-2 justify-content-end actions">
-            <i role="button" class="bi bi-pencil"></i>
+            {task.id === editingNoteId ? (
+              <span role="button" onClick={() => handleUpdateNote(task.id)}>
+                save
+              </span>
+            ) : (
+              <i
+                role="button"
+                className="bi bi-pencil"
+                onClick={() => {
+                  handleEdit(task.id);
+                }}
+              ></i>
+            )}
             <i
               role="button"
               className="bi bi-trash pointer-event"
