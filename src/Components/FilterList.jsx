df0645e577a942d4c730b768b6b89a7df0e59d8f
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-10-24 20:02:55 +0700

    Use props to handle button filter active

--- src/Components/FilterList.jsx
+++ src/Components/FilterList.jsx
@@ -1,16 +1,16 @@
-export default function FilterList() {
+export default function FilterList({filter, handleFilter}) {
     return (
         <div className="row py-5">
             <div className="col">
                 <ul className="nav nav-pills justify-content-center">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">All</a>
+                        <a className={`nav-link ${filter === 'All' ? 'active' : ''}`} aria-current="page" href="#" onClick={()=> handleFilter('All')}>All</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Active</a>
+                        <a className={`nav-link ${filter === 'Active' ? 'active' : ''}`} href="#" onClick={()=> handleFilter(('Active'))}>Active</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Completed</a>
+                        <a className={`nav-link ${filter === 'Completed' ? 'active' : ''}`} href="#" onClick={()=> handleFilter('Completed')}>Completed</a>
                     </li>
                 </ul>
             </div>
