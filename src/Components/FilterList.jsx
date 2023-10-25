b0c443235fee07e6c45aa5752890e94889457d39
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-10-25 15:02:27 +0700

    Handel the filter list, when in editing note mode

--- src/Components/FilterList.jsx
+++ src/Components/FilterList.jsx
@@ -1,19 +1,59 @@
-export default function FilterList({filter, handleFilter}) {
-    return (
-        <div className="row py-5">
-            <div className="col">
-                <ul className="nav nav-pills justify-content-center">
-                    <li className="nav-item">
-                        <a className={`nav-link ${filter === 'All' ? 'active' : ''}`} aria-current="page" href="#" onClick={()=> handleFilter('All')}>All</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className={`nav-link ${filter === 'Active' ? 'active' : ''}`} href="#" onClick={()=> handleFilter(('Active'))}>Active</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className={`nav-link ${filter === 'Completed' ? 'active' : ''}`} href="#" onClick={()=> handleFilter('Completed')}>Completed</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
+export default function FilterList({
+  filter,
+  handleFilter,
+  isFinishEdit,
+  editingNoteId,
+}) {
+  return (
+    <div className="row py-5">
+      <div className="col">
+        <ul className="nav nav-pills justify-content-center">
+          <li className="nav-item">
+            <button
+              className={`nav-link ${filter === "All" ? "active" : ""}`}
+              aria-current="page"
+              onClick={() => {
+                if (isFinishEdit && editingNoteId === null) {
+                  handleFilter("All");
+                }
+              }}
+            >
+              All
+            </button>
+          </li>
+          <li className="nav-item">
+            <button
+              className={`nav-link ${filter === "Active" ? "active" : ""}`}
+              onClick={() => {
+                if (isFinishEdit && editingNoteId === null) {
+                  handleFilter("Active");
+                }
+              }}
+            >
+              Active
+            </button>
+          </li>
+          <li className="nav-item">
+            <button
+              className={`nav-link ${filter === "Completed" ? "active" : ""}`}
+              onClick={() => {
+                if (isFinishEdit && editingNoteId === null) {
+                  handleFilter("Completed");
+                }
+              }}
+            >
+              Completed
+            </button>
+          </li>
+        </ul>
+      </div>
+      {editingNoteId !== null ? (
+          <span className="text-danger text-center w-100 py-1" style={{ fontSize: 12 }}>
+            When editing note can't filter note
+          </span>
+        ) : (
+          ""
+        )}
+    </div>
+  );
 }
